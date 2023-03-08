eb52e997a57b7ae845c38363c5bd7c7e43d8bed5
Arthur <ar3tyr7@gmail.com>
2023-03-08 23:53:22 +0300

    work4

---
+++ src/main/java/Work4/Main.java
@@ -0,0 +1,14 @@
+package Work4;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        MyLinkedList<Integer> numbers = new MyLinkedList<>();
+
+        numbers.add(4);
+        numbers.add(8);
+        numbers.add(35);
+        System.out.println(numbers);
+    }
+}
---
+++ src/main/java/work3/main.java
@@ -0,0 +1,9 @@
+package work3;
+
+import java.util.LinkedList;
+
+public class main {
+    public static void main(String[] args) {
+        LinkedList<String> s = new LinkedList<>();
+    }
+}
